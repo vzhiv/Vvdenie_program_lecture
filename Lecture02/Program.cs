97f8369c54458dab03f77a8278d4a839f6f7eda2
vzhiv <vzhiv@fvis.ru>
2022-12-25 23:45:22 +0300

    Add data Lecture02

--- Lecture02/Program.cs
+++ Lecture02/Program.cs
@@ -1,1 +1,106 @@
-﻿// Лекция 02 Функции
+﻿// Лекция 02 Функции в языке C# - методы
+
+
+/*
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+//int a1 = 1, a2 = 2, a3 = 45543;
+//int b1 = 4, b2 = 155, b3 = 6455;
+//int c1 = 7, c2 = 8, c3 = 9;
+
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+
+
+/*
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+
+int max = Max(max1, max2, max3);
+
+
+//int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+
+Console.WriteLine(max);
+
+
+
+//
+
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+*/
+
+
+
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index<length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int [] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
